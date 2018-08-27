dcb41995d97c89eb3c20b1bc19208b4cf5c08fab
Adam Oliveras <theadamoliveras@gmail.com>
2018-08-27 14:50:22 -0700

    moved timer, submit, mark and bombcount out of gameboard and into boardwrapper for logic purposes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 console.log('JS Loadeeeeee!!!')
+
+
+//function main () {
+
+     //document.getElementsByClassName("playBox");
+    //console.log(chosenSquare);
+
+    let flag = "<img src='./images/flag.png'>";
+    let possibleMarks = [flag, "?"]
+
+    //function clickedSquare() {
+        //console.log();
+    //};
+
+    function changeHTML() {
+        event.preventDefault();
+        let chosenSquare = event.target;
+        chosenSquare.innerHTML = possibleMarks[0];
+        //chosenSquare.innerText = possibleMarks[m];
+    }
+
+    document.querySelector('.gameboard').addEventListener('click', e => {
+        //e.target.innerText = "X";
+        e.preventDefault();
+        (e.button === 0 ? e.target.innerText = "X" : e.target.innerHTML = possibleMarks[0]);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
+
+
+//main();
