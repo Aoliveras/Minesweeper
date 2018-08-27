4062b259d41dbe7f91a28d65c2402f45528acb23
Adam Oliveras <theadamoliveras@gmail.com>
2018-08-27 16:29:06 -0700

    tweaked logic so that right click results cannot be manioulated by left click

--- js/main.js
+++ js/main.js
@@ -13,23 +13,35 @@ console.log('JS Loadeeeeee!!!')
         //console.log();
     //};
 
-    function changeHTML() {
+    /*function changeHTML() {
         event.preventDefault();
         let chosenSquare = event.target;
         chosenSquare.innerHTML = possibleMarks[0];
         //chosenSquare.innerText = possibleMarks[m];
-    }
+    }*/
 
     document.querySelector('.gameboard').addEventListener('contextmenu', e => {
-        e.preventDefault();
-        e.target.innerText = "X";
+        
+        //e.target.innerText === "" ? e.target.innerHTML = possibleMarks[0] : e.target.innerHTML = "";
+        if (e.target.innerText === "") {
+            e.preventDefault(); 
+            e.target.innerText = "B!";
+            } else if (e.target.innerText === "B!") {
+                e.preventDefault();
+                e.target.innerText = "?";
+                } else if (e.target.innerText === "?") {
+                    e.preventDefault();
+                    e.target.innerText = "";
+                } else if (e.target.innerText === "X") {
+                    e.preventDefault();
+                } else e.preventDefault();
     });
 
     document.querySelector('.gameboard').addEventListener('click', e => {
         //e.target.innerText = "X";
         var whichButton = e.button;
         console.log(whichButton);
-        e.target.button === 2 ? e.target.innerText = "X" : e.target.innerHTML = possibleMarks[0];
+        e.target.innerText === "" ? e.target.innerText = "X": e.preventDefault();
     });
 
 
